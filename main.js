ef7b788d51dbc574efe5f33fc8be545bd37838f8
natsm <natsmarinho@gmail.com>
2023-10-12 18:25:50 -0300

    style: icone de error e borda do input mudam de cor se tiver erro

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const email = document.querySelector("#in-email");
 const btnRequest = document.querySelector("#btn-request").addEventListener("click", requested);
 const msgError = document.querySelector(".error-msg");
 const emailValidate = document.querySelector(".valid-msg");
+const errorIcon = document.querySelector(".img-error");
 const regexEmail = /^[^\.\s][\w\-]+(\.[\w\-]+)*@([\w-]+\.)+[\w-]{2,}$/;
 
 function setError() {
    msgError.style.display = "block";
+   email.style.border = "2px solid red";
+   errorIcon.style.display = "inline-block";
 }
 
 function removeError() {
     msgError.style.display = "none";
+    email.style.border = "px solid #CE9898";
+    errorIcon.style.display = "none";
 }
 
 function emailValido() {
